0fb80ac8258751e1fbf023398a9bb064f4ceb1a2
Allen Day <allenday@users.noreply.github.com>
2022-10-27 15:25:11 +0800

    formatting

--- ethereum/ether-supply-by-day.sql
+++ ethereum/ether-supply-by-day.sql
@@ -1,8 +1,8 @@
-with ether_emitted_by_date  as (
-  select date(block_timestamp) as date, sum(value) as value
-  from `bigquery-public-data.crypto_ethereum.traces`
-  where trace_type in ('genesis', 'reward')
-  group by date(block_timestamp)
+WITH ether_emitted_by_date  AS (
+  SELECT date(block_timestamp) AS date, SUM(value) AS value
+  FROM `bigquery-public-data.crypto_ethereum.traces`
+  WHERE trace_type IN ('genesis', 'reward')
+  GROUP BY DATE(block_timestamp)
 )
-select date, sum(value) OVER (ORDER BY date) / power(10, 18) AS supply
-from ether_emitted_by_date
+SELECT date, SUM(value) OVER (ORDER BY date) / POWER(10, 18) AS supply
+FROM ether_emitted_by_date
